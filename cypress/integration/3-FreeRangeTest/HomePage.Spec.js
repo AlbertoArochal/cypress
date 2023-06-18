119e3b701ccf8a1600ca2ee2a5e64d3a6225cf4b
AlbertoArochal <arochaldev@gmail.com>
2023-06-18 23:16:51 +0200

    added logged project

--- cypress/integration/3-FreeRangeTest/HomePage.Spec.js
+++ cypress/integration/3-FreeRangeTest/HomePage.Spec.js
@@ -1,8 +1,13 @@
 describe("home de www.freerangetesters.com", () => {
-  beforeEach(() => {
-    cy.visit("https://www.freerangetesters.com/");
-  });
-  it("The title should include free range testers", () => {
-    cy.title().should("include", "Free Range Testers");
-  });
+    beforeEach(() => {
+        cy.visit("https://www.freerangetesters.com/");
+    });
+    it("The title should include free range testers", () => {
+        cy.title().should("include", "Free Range Testers");
+    });
+    it("should navigate to other section when I click on it", () => {
+        cy.get(
+            '#comp-krjarw4p > [data-testid="linkElement"] > .M3I7Z2'
+        ).click();
+    });
 });
